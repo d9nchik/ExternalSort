0c65eeec16fc7b860db85071e7387e83133d5ab8
d9nich <dan.galaiko@gmail.com>
2020-09-02 15:40:30 +0300

    ReverseSequentGenerator has been added.

--- src/com/d9nich/generator/ReverseSequentGenerator.java
+++ src/com/d9nich/generator/ReverseSequentGenerator.java
@@ -1,15 +1,17 @@
 package com.d9nich.generator;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class SequentGenerator implements Generatable {
-
+public class ReverseSequentGenerator implements Generatable {
     @Override
     public void generate(String fileName, int dataSize) {
-        int number = Integer.MIN_VALUE;
+        int number = Integer.MAX_VALUE;
         try (DataOutputStream output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
             for (int i = 0; i < dataSize; i++)
-                output.writeInt(number++);
+                output.writeInt(number--);
         } catch (IOException e) {
             e.printStackTrace();
         }
