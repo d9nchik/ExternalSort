832098f10619058e742350cc2334e41cee62497a
d9nich <dan.galaiko@gmail.com>
2020-09-02 16:03:42 +0300

    Beginning realization of FileSortable interface.

---
+++ src/com/d9nich/exuteranlSort/DirectMerge.java
@@ -0,0 +1,4 @@
+package com.d9nich.exuteranlSort;
+
+public class DirectMerge {
+}
---
+++ src/com/d9nich/exuteranlSort/FileSortable.java
@@ -0,0 +1,5 @@
+package com.d9nich.exuteranlSort;
+
+public interface FileSortable {
+    void sort(String fileName);
+}
