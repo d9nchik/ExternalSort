217e3c2d0b94bce9f6075a2319e40fe0930147f7
d9nich <dan.galaiko@gmail.com>
2020-09-06 20:58:06 +0300

    Hot improve has been done.

--- src/com/d9nich/exteranlSort/SortLargeFile.java
+++ src/com/d9nich/exteranlSort/SortLargeFile.java
@@ -6,7 +6,8 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class SortLargeFile {
-    public static final int MAX_ARRAY_SIZE = 500_000_000;//set 2 to test small.dat
+    public static final int SIZE_OF_INT = 4;
+    public static final int MAX_ARRAY_SIZE = 2;//set 2 to test small.dat
     public static final int BUFFER_SIZE = 300_000 * 1_024;
 
     /**
@@ -16,7 +17,7 @@ public class SortLargeFile {
         // Implement Phase 1: Create initial segments
         int numberOfSegments = initializeSegments(sourcefile);
         // Implement Phase 2: Merge segments recursively
-        merge(numberOfSegments, MAX_ARRAY_SIZE, "f1.dat", "f2.dat", "f3.dat", targetfile);
+        merge(numberOfSegments, MAX_ARRAY_SIZE, "f1.dat", "f2.dat", targetfile);
     }
 
     /**
@@ -46,7 +47,7 @@ public class SortLargeFile {
 //        System.out.println(buffer.isLoaded());  //prints false
 //        System.out.println(buffer.capacity());  //Get the size based on content size of file
 
-            IntBuffer intBuffer = buffer.asIntBuffer();
+            IntBuffer intBuffer = buffer.asIntBuffer();//TODO: refactor
             IntBuffer outputIntBuffer = outputBuffer.asIntBuffer();
 
             //You can read the file from this buffer the way you like.
@@ -72,11 +73,11 @@ public class SortLargeFile {
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored ")
-    private static void merge(int numberOfSegments, int segmentSize, String f1, String f2, String f3, String targetfile)
+    private static void merge(int numberOfSegments, int segmentSize, String f1, String f2, String targetfile)
             throws Exception {
         if (numberOfSegments > 1) {
-            mergeOneStep(numberOfSegments, segmentSize, f1, f2, f3);
-            merge((numberOfSegments + 1) / 2, segmentSize * 2, f3, f1, f2, targetfile);
+            mergeOneStep(numberOfSegments, segmentSize, f1, f2);
+            merge((numberOfSegments + 1) / 2, segmentSize * 2, f2, f1, targetfile);
         } else {
             // Rename f1 as the final sorted file
             File sortedFile = new File(targetfile);
@@ -85,22 +86,30 @@ public class SortLargeFile {
         }
     }
 
-    private static void mergeOneStep(int numberOfSegments, int segmentSize, String f1, String f2, String f3)
+    private static void mergeOneStep(int numberOfSegments, int segmentSize, String inputFile, String outputFile)
             throws Exception {//TODO: change to try-with-resources
-        DataInputStream f1Input = new DataInputStream(new BufferedInputStream(new FileInputStream(f1), BUFFER_SIZE));
-        DataOutputStream f2Output = new DataOutputStream(new BufferedOutputStream(
-                new FileOutputStream(f2), BUFFER_SIZE));// Copy half number of segments from f1.dat to f2.dat
-        copyHalfToF2(numberOfSegments, segmentSize, f1Input, f2Output);
-        f2Output.close();
-        // Merge remaining segments in f1 with segments in f2 into f3
-        DataInputStream f2Input = new DataInputStream(
-                new BufferedInputStream(new FileInputStream(f2), BUFFER_SIZE));
-        DataOutputStream f3Output = new DataOutputStream(
-                new BufferedOutputStream(new FileOutputStream(f3), BUFFER_SIZE));
-        mergeSegments(numberOfSegments / 2, segmentSize, f1Input, f2Input, f3Output);
+        final long SKIP_BYTES = (numberOfSegments / 2) * segmentSize * SIZE_OF_INT;
+        RandomAccessFile f1Input = new RandomAccessFile(inputFile, "r");
+        f1Input.seek(0);
+        final FileChannel f1InputChannel = f1Input.getChannel();
+        IntBuffer f1Buffer = f1InputChannel.map(FileChannel.MapMode.READ_ONLY, 0, SKIP_BYTES).asIntBuffer();
+
+        RandomAccessFile f2Input = new RandomAccessFile(inputFile, "r");
+        f2Input.seek(SKIP_BYTES);
+        final long TOTAL_SIZE = f1InputChannel.size();
+        IntBuffer f2Buffer = f2Input.getChannel().map(FileChannel.MapMode.READ_ONLY, SKIP_BYTES,
+                TOTAL_SIZE - SKIP_BYTES).asIntBuffer();
+
+        // Copy half number of segments from inputFile.dat to output.dat
+        // Merge remaining segments in inputFile with segments in output into f3
+        RandomAccessFile output = new RandomAccessFile(outputFile, "rw");
+        output.setLength(0);
+        IntBuffer outputBuffer = output.getChannel().map(FileChannel.MapMode.READ_WRITE, 0,
+                TOTAL_SIZE).asIntBuffer();
+        mergeSegments(numberOfSegments / 2, segmentSize, f1Buffer, f2Buffer, outputBuffer);
         f1Input.close();
         f2Input.close();
-        f3Output.close();
+        output.close();
     }
 
     /**
@@ -116,49 +125,54 @@ public class SortLargeFile {
     /**
      * Merge all segments
      */
-    private static void mergeSegments(int numberOfSegments, int segmentSize, DataInputStream f1, DataInputStream f2,
-                                      DataOutputStream f3) throws Exception {
+    private static void mergeSegments(int numberOfSegments, int segmentSize, IntBuffer f1, IntBuffer f2,
+                                      IntBuffer f3) {
         for (int i = 0; i < numberOfSegments; i++)
             mergeTwoSegments(segmentSize, f1, f2, f3);
 
         // If f1 has one extra segment, copy it to f3
-        while (f1.available() > 0)
-            f3.writeInt(f1.readInt());
+        while (available(f2)) {
+            f3.put(f2.get());
+        }
+
     }
 
     /**
      * Merges two segments
      */
-    private static void mergeTwoSegments(int segmentSize, DataInputStream f1, DataInputStream f2, DataOutputStream f3)
-            throws Exception {
-        int intFromF1 = f1.readInt();
-        int intFromF2 = f2.readInt();
+    private static void mergeTwoSegments(int segmentSize, IntBuffer f1, IntBuffer f2, IntBuffer f3) {
+        int intFromF1 = f1.get();
+        int intFromF2 = f2.get();
         int f1Count = 1;
         int f2Count = 1;
         while (true) {
             if (intFromF1 < intFromF2) {
-                f3.writeInt(intFromF1);
-                if (f1.available() == 0 || f1Count++ >= segmentSize) {
-                    f3.writeInt(intFromF2);
+                f3.put(intFromF1);
+                if (!available(f1) || f1Count++ >= segmentSize) {
+                    f3.put(intFromF2);
                     break;
                 } else {
-                    intFromF1 = f1.readInt();
+                    intFromF1 = f1.get();
                 }
             } else {
-                f3.writeInt(intFromF2);
-                if (f2.available() == 0 || f2Count++ >= segmentSize) {
-                    f3.writeInt(intFromF1);
+                f3.put(intFromF2);
+                if (!available(f2) || f2Count++ >= segmentSize) {
+                    f3.put(intFromF1);
                     break;
                 } else {
-                    intFromF2 = f2.readInt();
+                    intFromF2 = f2.get();
                 }
             }
         }
-        while (f1.available() > 0 && f1Count++ < segmentSize) {
-            f3.writeInt(f1.readInt());
+        while (available(f1) && f1Count++ < segmentSize) {
+            f3.put(f1.get());
         }
-        while (f2.available() > 0 && f2Count++ < segmentSize) {
-            f3.writeInt(f2.readInt());
+        while (available(f2) && f2Count++ < segmentSize) {
+            f3.put(f2.get());
         }
+    }
+
+    private static boolean available(IntBuffer buffer) {
+        return buffer.position() < buffer.limit();
     }
 }
